83bd48113a5e5b5f7ad31f9822054c0d6428a447
mkalme <mikelis.kalme@gmail.com>
2025-05-22 18:10:06 +0300

    Add project files.

---
+++ TicTacToe/Game.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TicTacToe
+{
+    public class Game
+    {
+        public string[,] Grid { get; set; } = new string[3, 3];
+        public string CurrentRound { get; set; } = "O";
+
+        public void PlayRound(string cords) {
+            cords = cords.ToLower();
+
+            int x = GridDictionary[cords[0]];
+            int y = Int32.Parse(cords[1].ToString());
+
+            Grid[x, y] = CurrentRound;
+
+            CurrentRound = GetNextRound();
+        }
+        private static Dictionary<char, int> GridDictionary = new Dictionary<char, int>() {
+            {'a', 0 },
+            {'b', 1 },
+            {'c', 2 },
+        };
+        private string GetNextRound() {
+            if (CurrentRound == "O") return "X";
+            return "O";
+        }
+
+        public WinnerType GetWinner() {
+            List<Line> lines = Line.GetListFromGrid(Grid);
+
+            foreach (Line line in lines) {
+                WinnerType winner = line.GetWinner();
+
+                if (winner == WinnerType.X || winner == WinnerType.O) return winner;
+            }
+
+            //Check for tie
+            for (int y = 0; y < 3; y++) {
+                for (int x = 0; x < 3; x++) {
+                    if (string.IsNullOrEmpty(Grid[x, y])) return WinnerType.None;
+                }
+            }
+
+            return WinnerType.Tie;
+        }
+    }
+
+    struct Line {
+        public string[] Cells { get; set; }
+
+        public Line(params string[] cells) {
+            Cells = cells;
+        }
+
+        public static List<Line> GetListFromGrid(string[,] grid){
+            Line l0 = new Line(grid[0, 0], grid[1, 0], grid[2, 0]);
+            Line l1 = new Line(grid[2, 0], grid[2, 1], grid[2, 2]);
+            Line l2 = new Line(grid[0, 2], grid[1, 2], grid[2, 2]);
+            Line l3 = new Line(grid[0, 0], grid[0, 1], grid[0, 2]);
+            Line l4 = new Line(grid[0, 1], grid[1, 1], grid[2, 1]);
+            Line l5 = new Line(grid[1, 0], grid[1, 1], grid[1, 2]);
+            Line l6 = new Line(grid[0, 0], grid[1, 1], grid[2, 2]);
+            Line l7 = new Line(grid[0, 2], grid[1, 1], grid[2, 0]);
+
+            return new List<Line>() { l0, l1, l2, l3, l4, l5, l6, l7 };
+        }
+        public WinnerType GetWinner() {
+            string sum = String.Join("", Cells);
+            if (sum != "OOO" && sum != "XXX") return WinnerType.None;
+
+            if (sum == "OOO") return WinnerType.O;
+            return WinnerType.X;
+        }
+    }
+
+    public enum WinnerType { 
+        O,
+        X,
+        Tie,
+        None
+    }
+}
---
+++ TicTacToeUI/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using TicTacToe;
+
+namespace TicTacToeUI {
+    class Program {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            
+            WinnerType winner = WinnerType.None;
+            while (winner == WinnerType.None) {
+                Console.Clear();
+                DisplayGrid(game.Grid);
+                
+                Console.Write("Put " + game.CurrentRound + ": ");
+                game.PlayRound(Console.ReadLine());
+
+                winner = game.GetWinner();
+            }
+            DisplayWinner(game, winner);
+
+            Console.ReadLine();
+        }
+
+        private static void DisplayGrid(string[,] grid) {
+            string seperator = "|";
+
+            Console.WriteLine("   a b c");
+            for (int y = 0; y < 3; y++) {
+                string line = y + " ";
+
+                for (int x = 0; x < 3; x++) {
+                    string c = grid[x, y];
+                    c = string.IsNullOrEmpty(c) ? " " : c;
+
+                    line += seperator + c;
+                }
+                line += seperator;
+
+                Console.WriteLine(line);
+            }
+        }
+        private static void DisplayWinner(Game game, WinnerType winner)
+        {
+            Console.Clear();
+            DisplayGrid(game.Grid);
+
+            string message;
+
+            if (winner == WinnerType.O) message = "The O won!";
+            else if (winner == WinnerType.X) message = "The X won!";
+            else if (winner == WinnerType.Tie) message = "It's a tie!";
+            else message = "No one won";
+
+            Console.WriteLine(message);
+        }
+    }
+}
